74f60333f40ad6c5d4addde040fccb428369543c
AELHAKIMI <ayo-voi@hotmail.com>
2018-08-26 20:46:29 +0100

    Home 26/08/2018

--- archive/urls.py
+++ archive/urls.py
@@ -6,8 +6,23 @@ urlpatterns = [
     #/archive/
     url(r'^$', views.index , name='index'),
 
-    #/archive/229/
-    url(r'^(?P<patient_id>[0-9]+)/$', views.detailPatient, name='detailpatient')
+    #/archive/Patient/
+    url(r'^Patient/$', views.Patientindex, name='Patientindex'),
+
+    #/archive/Pateint/229/
+    url(r'^Patient/(?P<patient_id>[0-9]+)/$', views.detailPatient, name='detailpatient'),
+
+    #/archive/dossierpatient/
+    url(r'^dossierPatient/$', views.Dossierindex, name='dossierindex'),
+
+    #/archive/dossierpatient/223/
+    url(r'^dossierPatient/(?P<dossierpatient_id>[0-9]+)/$', views.detailDossierPatient, name='detaildossier'),
+
+    #/archive/Service/
+    url(r'^Service/$',views.ServiceIndex, name='Serviceindex' ),
+
+    #/archive/Service/334/
+    url(r'^Service/(?P<service_id>[0-9]+)/$', views.detailService, name='detailService')
 
 ]
 
--- archive/views.py
+++ archive/views.py
@@ -6,16 +6,66 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
+    return render(request, 'index.html' )
+
+
+def Dossierindex(request):
+    all_dossier = dossierPatient.objects.all()    
+    context = {'all_dossier' : all_dossier}
+    return render(request , 'dossierPatient/index.html', context)
+
+
+def Patientindex(request):
     all_patients = patient.objects.all()    
     context = {'all_patients' : all_patients}
-    return render(request , 'patient/index.html', context)
+    return render(request , 'Patient/index.html', context)
+
+
+def ServiceIndex(request):
+    all_Services = service.objects.all()
+    context = {'all_Services': all_Services}
+    return render(request, 'Service/index.html', context)
 
 
 def detailPatient(request, patient_id):
     try:
-        Patient = patient.objects.get(patientID = patient_id)        
+        PatientRow = patient.objects.get(patientID = patient_id)
+        titles = ('N', 'Patient ID', 'Patient Name')
+        Patient = (PatientRow.id, PatientRow.patientID, PatientRow.patientName) 
+        context ={
+            'the_row_title' : titles,
+            'Patient'        : Patient
+        }  
+             
     except patient.DoesNotExist:
         raise Http404('Patient does Not Exist')    
-    return render(request , 'patient/detail.html', {'Patient' : Patient} )
+    return render(request , 'Patient/detail.html', context ) 
+
+def detailDossierPatient(request, dossierpatient_id):
+    try:
+        dossierpatient = dossierPatient.objects.get(id = dossierpatient_id)
+        titles = ('Number', 'Dossier ID', 'Patient', 'Service')
+        dossier = (dossierpatient.id, dossierpatient.DossierID, dossierpatient.patientID  , dossierpatient.serviceID)
+        context = {
+            'the_row_title' : titles,
+            'dossier'   : dossier
+        } 
+               
+    except dossierPatient.DoesNotExist:
+        raise Http404('dossier does Not Exist')    
+    return render(request , 'dossierPatient/detail.html', context )
+
+def detailService(request, service_id):
+    try:
+        ServiceRow = service.objects.get(id = service_id)
+        titles = ('N', 'Service ID', 'Service')
+        Service = (ServiceRow.id, ServiceRow.serviceID, ServiceRow.service)
+        context ={
+            'the_row_title' : titles,
+            'Service'       : Service
+        }
+    except service.DoesNotExist:
+        raise Http404('Service Does Not Exist')
+    return render(request , 'Service/detail.html', context)
 
     
